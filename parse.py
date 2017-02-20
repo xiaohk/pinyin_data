870f63aa3e7a93c786a8db9f3bfe59f3ab0ab643
Jay Wong <xiao.hk1997@gmail.com>
2017-02-20 09:39:37 -0600

    Finish the initial Parser

---
+++ parse.py
@@ -0,0 +1,52 @@
+from yaml import dump as yaml_dump
+from json import dump as json_dump
+import argparse
+
+# Command line argument setting
+parser = argparse.ArgumentParser(description='Parse some pinyin.')
+parser.add_argument('-y', '--yaml', action = 'store_true', help = "Output to" + 
+                    "a YAML file")
+parser.add_argument('-j', '--json', action = 'store_true', help = "Output to" +
+                    "a JSON file")
+args = parser.parse_args()
+
+# Parse from the DB
+hanzi = dict()
+
+def format_u(capital):
+    # Use 32-bit
+    escape = "\\U" + capital[2:].zfill(8)
+    return escape.encode('ascii').decode('unicode-escape')
+
+# Read from the file
+with open("unihan/Unihan_Readings.txt", 'r') as fp:
+    lines = fp.readlines()
+    for line in lines:
+        words = line.split()
+
+        # Comment line or empty line
+        if (len(words) < 3 or words[0] == '#'):
+            continue
+
+        uni = format_u(words[0])
+        # The most common used reading, keep it as the first entry of value list
+        if words[1] == "kMandarin":
+            hanzi[uni] = [words[2]]
+
+        elif words[1] == "kXHC1983" or words[1] == "kHanyuPinyin":
+            pinyin_list = words[2][words[2].find(':') + 1 : ].split(',')
+            if uni in hanzi:
+                new_set = set(hanzi[uni])
+                new_set.update(pinyin_list)
+                hanzi[uni] = list(new_set)
+            else:
+                hanzi[uni] = pinyin_list 
+
+# Dump the dictionary
+if args.yaml:
+    with open("data/pinyin.yaml", 'w') as stream:
+        yaml_dump(hanzi, stream)
+
+if (not args.json and not args.yaml) or args.json:
+    with open("data/pinyin.json", 'w') as stream:
+        json_dump(hanzi, stream, indent=4)
