87b59e8f02c5eb304e24c6c51aea4206de455874
Jay Wong <xiao.hk1997@gmail.com>
2017-02-26 10:30:00 -0600

    Fix the kXHC1983 parsing bug

--- parse.py
+++ parse.py
@@ -5,9 +5,11 @@ import argparse
 # Command line argument setting
 parser = argparse.ArgumentParser(description='Parse some pinyin.')
 parser.add_argument('-y', '--yaml', action = 'store_true', help = "Output to" + 
-                    "a YAML file")
+                    " a YAML file")
 parser.add_argument('-j', '--json', action = 'store_true', help = "Output to" +
-                    "a JSON file")
+                    " a JSON file")
+parser.add_argument('-d', '--debug', action = 'store_true', help = "Do not" +
+                   " dump to files")
 args = parser.parse_args()
 
 # Parse from the DB
@@ -18,6 +20,15 @@ def format_u(capital):
     escape = "\\U" + capital[2:].zfill(8)
     return escape.encode('ascii').decode('unicode-escape')
 
+def update_pinyin(uni, pinyin_list, result):
+    if uni in result:
+        # Can't directly update set, want to keep kMandarin as 
+        # the first
+        diff_set = set(pinyin_list) - set(result[uni])
+        result[uni] = result[uni] + list(diff_set)
+    else:
+        result[uni] = pinyin_list 
+
 def parse_pinyin():
     # Read from the file
     with open("unihan/Unihan_Readings.txt", 'r') as fp:
@@ -26,7 +37,7 @@ def parse_pinyin():
             words = line.split()
 
             # Comment line or empty line
-            if (len(words) < 3 or words[0] == '#'):
+            if (len(words) < 2 or words[0] == '#'):
                 continue
 
             uni = format_u(words[0])
@@ -38,24 +49,25 @@ def parse_pinyin():
                         result[uni] = [words[2]] + result[uni]
                 else:            
                     result[uni] = [words[2]]
-
-            elif words[1] == "kXHC1983" or words[1] == "kHanyuPinyin":
+            
+            elif words[1] == "kXHC1983":
+                pinyin_list = [p[p.find(':') + 1:] for p in words[2:]]
+                update_pinyin(uni, pinyin_list, result)
+                
+            elif words[1] == "kHanyuPinyin":
                 pinyin_list = words[2][words[2].find(':') + 1 : ].split(',')
-                if uni in result:
-                    # Can't directly update set, want to keep kMandarin as 
-                    # the first
-                    diff_set = set(pinyin_list) - set(result[uni])
-                    result[uni] = result[uni] + list(diff_set)
-                else:
-                    result[uni] = pinyin_list 
+                update_pinyin(uni, pinyin_list, result)
+
+    return result
 
 #def parse_jyutping()
+result = parse_pinyin()
                 
 # Dump the dictionary
 if args.yaml:
     with open("data/pinyin.yaml", 'w') as stream:
         yaml_dump(result, stream)
 
-if (not args.json and not args.yaml) or args.json:
+if (not args.json and not args.yaml and not args.debug) or args.json:
     with open("data/pinyin.json", 'w') as stream:
         json_dump(result, stream, indent=4)
